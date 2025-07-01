673db867c5808e21e73267de30ffe5d08248470f
salsabillaz31 <salsaabillz04@gmail.com>
2025-07-02 03:32:14 +0700

    Add api

--- utils/llama_utils.py
+++ utils/llama_utils.py
@@ -5,7 +5,7 @@ import json
 from groq import Groq
 
 load_dotenv()
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+client = Groq(api_key="sk_neVud8MFlBCqE5ukVCxhWGdyb3FYq4j39qWNxcip488iLNsuFqn4")
 
 def create_aes_prompt(question, answer):
     return f"""
